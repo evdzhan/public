ed99d6b456d38226823f7f675f2d098ce972222e
Evdzhan <evdjoint@hotmail.com>
2014-02-05 21:33:24 +0000

    Added my academic work

---
+++ EvdzhanMustafa_Academic_Stuff/Sudoku Solver/src/CS21120/Main.java
@@ -0,0 +1,19 @@
+package CS21120;
+
+
+public class Main {
+
+	/** @author Evdzhan Nedzhib Mustafa  enm3@aber.ac.uk
+	 * 
+	 * Starts the program.
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+	 SudokuFrame sf=new SudokuFrame();
+	
+	
+	
+	} 
+
+}
